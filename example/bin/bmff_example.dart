7d95f9d881f1c027988749a6b544b856d8a32337
caijinglong-b-imac <cjl_spy@163.com>
2022-04-12 17:42:49 +0800

    Add example

---
+++ example/bin/bmff_example.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+import 'package:bmff/bmff.dart';
+import 'package:bmff/bmff_io.dart';
+
+void main() {
+  final assetPath = 'assets/compare_still_1.heic';
+  final file = File(assetPath);
+  final context = BmffIoContext(file);
+
+  final boxes = Bmff(context).convertToBox();
+  for (final box in boxes) {
+    print(box);
+  }
+}
