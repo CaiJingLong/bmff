75270197ab5e325ad3c6919d4d227fdc017fb13d
cjl_macbook <cjl_spy@163.com>
2022-04-12 20:50:29 +0800

    Update test.sh

--- test/bmff_test.dart
+++ test/bmff_test.dart
@@ -1,15 +1,28 @@
+import 'dart:io';
+
+import 'package:bmff/bmff.dart';
+import 'package:bmff/bmff_io.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of tests', () {
-    // final awesome = Awesome();
+  final assetFilePath = 'example/assets/compare_still_1.heic';
+  late Bmff bmff;
 
-    // setUp(() {
-    //   // Additional setup goes here.
-    // });
+  setUp(() {
+    final file = File(assetFilePath);
+    BmffIoContext context = BmffIoContext(file);
+    bmff = Bmff(context);
+  });
 
-    // test('First Test', () {
-    //   expect(awesome.isAwesome, isTrue);
-    // });
+  test('Test box count', () {
+    final box = bmff.decodeBox();
+    expect(box.length, equals(3));
+  });
+
+  test('Test box type', () {
+    final box = bmff.decodeBox();
+    expect(box[0].type, equals('ftyp'));
+    expect(box[1].type, equals('meta'));
+    expect(box[2].type, equals('mdat'));
   });
 }
