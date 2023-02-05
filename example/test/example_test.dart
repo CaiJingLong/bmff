dce1c6cad50f043447948bd3f9c412bdb31b8ef9
cjl_macbook <cjl_spy@163.com>
2023-02-05 15:05:43 +0800

    ✅ Add test

--- example/test/example_test.dart
+++ example/test/example_test.dart
@@ -1,1 +1,21 @@
-void main() {}
+import 'dart:typed_data';
+
+import 'package:bmff/bmff.dart';
+import 'package:test/expect.dart';
+import 'package:test/scaffolding.dart';
+
+void main() {
+  group('Test heic', () {
+    test('width and height', () {
+      final bmff = Bmff.file('assets/compare_still_1.heic');
+
+      final buffer = bmff['meta']['iprp']['ipco']['ispe'].getByteBuffer();
+
+      final width = buffer.getUint32(1, Endian.big);
+      final height = buffer.getUint32(2, Endian.big);
+
+      expect(width, equals(1440));
+      expect(height, equals(960));
+    });
+  });
+}
